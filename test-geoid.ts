dc3b197cf4297066d444b9ecc189d2d4ad1584b6
WatermarkPro <noelkiu@gmail.com>
2026-06-06 07:58:13 +0800

    chore: remove debug scripts

--- test-geoid.ts
+++
@@ -1,6 +0,0 @@
-import { getRowByGeoId } from "./lib/google-sheets";
-async function run() {
-  const row = await getRowByGeoId("G12197");
-  console.log("Row number:", row?.rowNumber);
-}
-run();
--- test-sheet6.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config({ path: ".env.local" });
-import { getRowByRowNumber } from "./lib/google-sheets";
-async function run() {
-  const row = await getRowByRowNumber(106, "Sheet6");
-  console.log("Sheet6 Row 106 ID:", row?.geoId);
-  console.log("Sheet6 Row 106 Summary:", row?.main);
-}
-run();
