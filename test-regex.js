bd4a89226bdc43d170cdd45c246ea505b4399610
WatermarkPro <noelkiu@gmail.com>
2026-06-06 09:02:44 +0800

    chore: remove regex test script

--- test-regex.js
+++
@@ -1,6 +0,0 @@
-const text = `G10473
-*BUSINESS FOR SALE*
-Business: Baking goods supply`;
-const regex = /^.*?\b(FOR\s+(SALE|LEASE|SALE\s*(AND|\/|&)\s*LEASE|SALE\/LEASE)|AVAILABLE|SOLD|LEASED OUT|OFF THE MARKET|ON HOLD|UNDER NEGO|UNDECISIVE SELLER)\b.*$/im;
-console.log('Match?', regex.test(text));
-console.log('Replaced:\n' + text.replace(regex, '*SOLD*'));
